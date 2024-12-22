3c590e36a9c6a8830e779b57a603cc97220ddd38
unknown <yasasbanukaofficial@gmail.com>
2024-12-22 11:48:42 +0530

    feat: added home page for the application

--- CourseWork.java
+++ CourseWork.java
@@ -3,13 +3,25 @@ class Example{
     // Easier to access the scanner from any method
     private static Scanner sc = new Scanner(System.in);
 
+    public static void options(int num, String optionTitle) {
+        System.out.println("[" + num + "]" + optionTitle);   
+    }
+
+    public static void processing(String optionTitle) {
+        System.out.print("Processing your request: " + optionTitle + "...");
+        delay();
+    }
+
     public static void loginValidator() {
-        //Login Style
+        // To clear the console
+        clearConsole();
+        //Login validator header style
         System.out.println();
         System.out.println("============================================================\n");
         System.out.printf("%43s", "Welcome to LibraryPro \n");
         System.out.println("\n============================================================");
         System.out.println();
+
         //Login Validator
         while (true) {
             System.out.print("Enter username:   ");
@@ -21,7 +33,9 @@ class Example{
                 case "admin":
                     switch (pass) {
                         case "admin123":
-                            System.out.println("Authenticating...");
+                            System.out.println();
+                            System.out.print("Authenticating...");
+                            delay();
                             clearConsole();
                             return;
                         default:
@@ -41,8 +55,67 @@ class Example{
             }
         }
     }
+
+    public static void homePage() {
+        int option = 0;
+        while (option != 6) {
+            //Home page header style
+            System.out.println();
+            System.out.println("+==========================================================+\n");
+            System.out.printf("%43s", "Library Managemet System \n");
+            System.out.println("\n+==========================================================+");
+            System.out.println();
+
+            options(1, "Manage Books");
+            options(2, "Manage Members");
+            options(3, "Issue Books");
+            options(4, "Return Books");
+            options(5, "View Reports");
+            options(6, "Exit");
+
+            System.out.println("+==========================================================+\n");
+
+            System.out.print("Please select an option (1-6):    ");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    processing("Manage Books");
+                    clearConsole();
+                    break;
+                case 2:
+                    processing("Manage Members");
+                    clearConsole();
+                    break; 
+                case 3:
+                    processing("Issue Books");
+                    clearConsole();
+                    break;
+                case 4:
+                    processing("Return Books");
+                    clearConsole();
+                    break; 
+                case 5:
+                    processing("View Reports");
+                    clearConsole();
+                    break;
+                case 6:
+                    processing("Exiting Application");
+                    clearConsole();
+                    break;   
+            
+                default:
+                    System.out.println("Error:  Invalid Option, Please Try again!");
+                    clearConsole();
+                    break;
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        loginValidator();
+        loginValidator(); //Login Process
+        homePage(); // Direct to the home page
     }
 
 
@@ -61,5 +134,13 @@ class Example{
         // Handle the exception
         System.err.println(e.getMessage());
         }
+    }
+    // add a delay affect 
+    public static void delay() {
+        int delayTime = Byte.MAX_VALUE * 75 ; // approx:    2 seconds
+        for (int i = 0; i < delayTime; i++) {
+            System.out.print("\b\b");
+            System.out.print("..");
         }
+    }
 }
